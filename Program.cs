97daee1e7f28388589ed883803dd05a699d13741
Simon Curtis <simon.curtis@candc-uk.com>
2023-05-24 14:54:19 +0100

    Change things around, updated UX

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using EpicGamesFreeGameFinder.Models;
+
+string query = File.ReadAllText("graphql.query");
+string variables = File.ReadAllText("variables.json");
+
+var baseUrl = "https://www.epicgames.com";
+var client = new HttpClient
+{
+    BaseAddress = new Uri(baseUrl)
+};
+
+var endpoint = $"graphql?query={query}&variables={variables}";
+var result = await client.GetAsync(endpoint);
+
+if (await result.Content.ReadFromJsonAsync<Root>() is not { } root)
+    return;
+
+var elements = root.data.Catalog.searchStore.elements;
+
+var stringBuilder = new StringBuilder();
+
+foreach (var group in elements.GroupBy(x => x.effectiveDate.Date).OrderByDescending(x => x.Key))
+{
+    stringBuilder.AppendFormat("## {0:dd MMM yyyy}\n\n", group.Key);
+    foreach (var element in group)
+    {
+        var slug = element.productSlug?
+            .Replace("/home", "")
+            .Replace("/", "--")
+            ?? element.title;
+
+        stringBuilder.AppendFormat("[{0}]({1}) \n\n", element.title, $"{baseUrl}/en-US/p/{slug}");
+    }
+}
+
+File.WriteAllText("output.md", stringBuilder.ToString());
---
+++ readme.md
@@ -0,0 +1,12 @@
+### EPIC Games Free Game Finder
+
+This code is designed to find the latest EPIC's free games, using their own API.
+
+_You will need .NET 6 SDK installed_
+
+To run this app:
+
+1. Clone this repo
+2. Change any variables (in `variables.json`) such as `effectiveDate`
+3. Open a terminal in the directory and run `dotnet restore`
+4. Run `dotnet run` and it will output (`output.md`) a markdown file with the games
